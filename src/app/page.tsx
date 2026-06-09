f1b219e13994573efcd9364234613c09be3c3fa9
ferreira-gn <geanoficial14@gmail.com>
2026-06-09 11:17:39 -0300

    chore: add shadcnui lib to project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button.tsx"
 
 export default function Home() {
   return (
-    <div className="flex flex-1 items-center justify-center " >
+    <div className="flex flex-1 flex-col items-center justify-center  " >
        <p>Hello world Rota Fácil</p>
+       <Button>Click me</Button>
     </div>
 );
 }
