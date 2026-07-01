d1ff882b989d1aee77c514bae4086d8843fd06e9
Thauã Gabriel <tagashi.dev@gmail.com>
2026-06-30 21:30:34 -0300

    refactor: adjusting tailwind use

---
+++ src/components/atom/SelectFilter.tsx
@@ -0,0 +1,9 @@
+export default function SelectFilter() {
+  return (
+    <div className="flex flex-col gap-2">
+      <div className="w-10 h-10 bg-red-600" />
+      <div className="w-20 h-10 bg-blue-600" />
+      <div className="w-50 h-10 bg-green-600" />
+    </div>
+  );
+}
